8b6110c4cc359ce4170a780cdebec8cf0276e926
Nicola Bellotti <n.bellotti90@gmail.com>
2018-01-29 00:10:06 +0100

    add button component and use inside modal component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,6 +81,10 @@ class BurgerBuilder extends Component {
 		this.setState({purchasing: false});
 	};
 	
+	purchaseContinueHandler = () => {
+		alert('You Continue')
+	};
+	
 	render () {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -92,7 +96,10 @@ class BurgerBuilder extends Component {
 		return (
 			<Aux>
 				<Modal show={this.state.purchasing} click={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients}/>
+					<OrderSummary
+						ingredients={this.state.ingredients}
+						purchaseContinued={this.purchaseContinueHandler}
+						purhcaseCancelled={this.purchaseCancelHandler}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls
