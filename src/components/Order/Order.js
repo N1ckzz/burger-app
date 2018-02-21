ba8765dac801eb3fded116c0c111b6baf9c0abbc
Nicola Bellotti <nbellotti@byte-code.com>
2018-02-21 15:09:31 +0100

    add and implement new /orders route and components

---
+++ src/components/Order/Order.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(key => {
+            return (
+                <span
+                    style={{
+                        textTransform: 'capitalize',
+                        display: 'inline-block',
+                        margin: '0 8px',
+                        border: '1px solid #ccc',
+                        padding: '5px',
+                        // boxShadow: '0 2px 3px #cccccc'
+                    }}
+                    key={key}>
+                    {key} : {props.ingredients[key]}
+                </span>
+            );
+        });
+
+    return (
+        <div className={classes.Order}>
+            <span>Ingredients:</span>
+            {ingredientSummary}
+            <p>Price: <strong>{props.totalPrice.toFixed(2)} €</strong></p>
+        </div>
+    );
+};
+
+export default order;
